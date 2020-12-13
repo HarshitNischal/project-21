5cbe3c70d1d5ed6d01596e41c8e7e5f69713aebd
HarshitNischal <71174516+HarshitNischal@users.noreply.github.com>
2020-12-13 19:48:32 +0530

    Add files via upload

--- Project 21/sketch.js
+++ Project 21/sketch.js
@@ -5,42 +5,39 @@ function setup() {
   createCanvas(1600,400);
 speed=random(55,90)
 weight=random(400,1500)
-bullet=createSprite(50,200,50,50);
+thickness=random(22,83)
+bullet=createSprite(50,200,50,20);
 bullet.shapeColor="white"
   wall=createSprite(1200,200,thickness,height/2);
   wall.shapeColor=(80,80,80)
   bullet.velocityX = speed;
-thickness=random(22,83)
+
 }
 
 function draw() 
 {
   background(255,255,255);  
- if(wall.x-bullet.x < (bullet.width+wall.width)/2)
+ if(wall.x-bullet.x < (bullet.width+wall.width)/2){
 
 bullet.velocityX=0;
-var deformation=0.5 * weight * speed* speed/22509; 
- if(deformation>180)
+var deformation=0.5 * weight * speed* speed/(thickness*thickness*thickness); 
+ if(deformation>10)
  {
     bullet.shapeColor=color(255,0,0);
  }
  
- if(deformation<180 && deformation>100)
- {
-    bullet.shapeColor=color(230,230,0);
- }
  
- if(deformation<100)
+ 
+ if(deformation<10)
  {
     bullet.shapeColor=color(0,255,0);
-     speed=random(223,321)
-     weight=random(30,52)
+    
  
  
  
  
   }
-
+ }
 
  
  
